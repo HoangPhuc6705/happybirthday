495ba90f6f096f34777026c8e958acbd95a18206
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2024-05-04 16:21:15 +0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const next_button = document.querySelector('.forward');
 const pre_button = document.querySelector('.back');
+const show_slide = document.querySelectorAll('.items');
 
 const children_element = document.querySelector('.main').children.length;
 let slide = [];
@@ -8,14 +9,60 @@ for (let i = 0; i < children_element; i++) {
 }
 let slideIndex = 0;
 
-next_button.addEventListener('click', () => {
-  
+if (slideIndex === 0) {
+  pre_button.disabled = true;
+  pre_button.style.visibility = 'hidden';
+  show_slide[slideIndex].style.right = '0%';
+}
 
-  console.log(slideIndex)
+next_button.addEventListener('click', () => {
+  slideIndex < children_element - 1 ? slideIndex++ : slideIndex;
+  show_slide[slideIndex].style.right = '0%';
+  show_slide[slideIndex].style.animation = 'nextSlide .5s ease-in-out';
+  show_slide[slideIndex - 1].style.animation = 'nextSlidePre .5s ease-in-out';
+
+  if (slideIndex === children_element - 1) {
+    next_button.disabled = true;
+    next_button.style.visibility = 'hidden';
+  } else {
+    next_button.disabled = false;
+    next_button.style.visibility = 'visible'
+  }
+
+  if (slideIndex > 0) {
+    pre_button.disabled = false;
+    pre_button.style.visibility = 'visible';
+  }
+
 })
 
 pre_button.addEventListener('click', () => {
-  
-  
-  console.log(slideIndex);
+  show_slide[slideIndex].style.right = '-100%';
+  show_slide[slideIndex].style.animation = 'preSlide .5s ease-in-out';
+  slideIndex > 0 ? slideIndex-- : slideIndex;
+  show_slide[slideIndex].style.animation = 'preSlideNext .5s ease-in-out';
+
+  if (slideIndex === 0) {
+    pre_button.disabled = true;
+    pre_button.style.visibility = 'hidden';
+  } else {
+    pre_button.disabled = false;
+    pre_button.style.visibility = 'visible';
+    next_button.disabled = false;
+    next_button.style.visibility = 'visible';
+  }
+
 })
+
+const background = [
+  '#C3E2C2',
+  '#EAECCC',
+  '#DBCC95',
+  '#CD8D7A',
+  '#C499F3',
+  '#31304D'
+]
+
+for (let i = 0; i < background.length; i++) {
+  show_slide[i].style.background = background[i];
+}
